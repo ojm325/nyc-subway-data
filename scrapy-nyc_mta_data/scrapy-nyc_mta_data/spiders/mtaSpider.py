25b82350399b31be415691e4ca0c5f8c94f9ea92
Omar Martinez <omar.martinez325@gmail.com>
2015-10-08 01:20:42 -0400

    Better filtering of NYC subway station data

--- scrapy-nyc_mta_data/scrapy-nyc_mta_data/spiders/mtaSpider.py
+++ scrapy-nyc_mta_data/scrapy-nyc_mta_data/spiders/mtaSpider.py
@@ -3,11 +3,11 @@ import logging
 from scrapy.selector import Selector
 
 class StackOverflowSpider(scrapy.Spider):
-    name = 'mta'
-    allowed_domains = ["mta.info"]
-    start_urls = ['http://web.mta.info/nyct/facts/ridership/ridership_sub_annual.htm']
+	name = 'mta'
+	allowed_domains = ["mta.info"]
+	start_urls = ['http://web.mta.info/nyct/facts/ridership/ridership_sub_annual.htm']
 
-    def parse(self, response):
+	def parse(self, response):
 		stations = response.xpath('//table[@id="subway"]/tr')
 		items = []
 		
@@ -16,31 +16,28 @@ class StackOverflowSpider(scrapy.Spider):
 			item = {}
 			station_info = {}
 
-			item['name'] = station.xpath('./td[1]/div/strong/text()').extract()
-			item['2009_ridership'] = station.xpath('./td[2]/text()').extract()
-			item['2010_ridership'] = station.xpath('./td[3]/text()').extract()
-			item['2011_ridership'] = station.xpath('./td[4]/text()').extract()
-			item['2012_ridership'] = station.xpath('./td[5]/text()').extract()
-			item['2013_ridership'] = station.xpath('./td[6]/text()').extract()
-			item['2014_ridership'] = station.xpath('./td[7]/text()').extract()
-			item['2013-14_diff'] = station.xpath('./td[8]/text()').extract()
-			item['change'] = station.xpath('./td[9]/text()').extract()
-			item['2014_rank'] = station.xpath('./td[10]/text()').extract()
+			item['name'] = ''.join(station.xpath('./td[1]/div/*/text()[1]').extract())
+			if((item['name'].isspace()) 
+				or (item['name'] == '')):
+				item['name'] = ''.join(station.xpath('./td[1]/div/*/a/text()').extract())
+
+				"""Make a function that Recursively go through to make sure that no name is empty"""
+			
+			item['2009_ridership'] = ''.join(station.xpath('./td[2]/text()').extract())
+			item['2010_ridership'] = ''.join(station.xpath('./td[3]/text()').extract())
+			item['2011_ridership'] = ''.join(station.xpath('./td[4]/text()').extract())
+			item['2012_ridership'] = ''.join(station.xpath('./td[5]/text()').extract())
+			item['2013_ridership'] = ''.join(station.xpath('./td[6]/text()').extract())
+			item['2014_ridership'] = ''.join(station.xpath('./td[7]/text()').extract())
+			item['2013-14_diff'] = ''.join(station.xpath('./td[8]/text()').extract())
+			item['change'] = ''.join(station.xpath('./td[9]/text()').extract())
+			item['2014_rank'] = ''.join(station.xpath('./td[10]/text()').extract())
 
 			station_info['station'] = item
 
-			if(item['name'] != []):
+			if(item['name'] != ''):
 				items.append(station_info)
 			
 			
 
 		return items
-
-    def parse_question(self, response):
-        yield {
-            'title': response.css('.title a::text').extract()[0],
-            'votes': response.css('.question .vote-count-post::text').extract()[0],
-            'body': response.css('.question .post-text').extract()[0],
-            'tags': response.css('.question .post-tag::text').extract(),
-            'link': response.url,
-        }
